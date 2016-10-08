571a9ec18568ec9f3402e84d9417a2fa41ec21f5
Spencer <spencer@branch2.com>
2016-10-08 01:21:52 -0500

    trying again

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use('/api', api); //sets the API used to access the Database
 //also to teach how Express and ejs work
 app.get('/', function(req, res, next) {
     
-    res.sendFile('index.html');
+    res.sendFile('./views/index.html');
 });
 
 
